da26c344d9eb4b8e4d02eb46c1f5be78513e6a8e
Nikolay <113996764+user175385803@users.noreply.github.com>
2023-11-19 21:46:25 +0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { Telegraf } from "telegraf";
+import { message } from "telegraf/filters";
+import config from "config";
+import { ogg } from "./ogg.js";
+
+const bot = new Telegraf(config.get('TG_TOKEN'));
+
+bot.on(message('voice'), async (ctx) => {
+  try {
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
+    const userId = String(ctx.message.from.id);
+    const oggPath = await ogg.create(link.href, userId);
+    const mp3Path = await ogg.toMp3(oggPath, userId);
+    await ctx.reply(mp3Path);
+  } catch (e) {
+    console.log('Error while voice message', e.message);
+  }
+})
+
+bot.command('star', async (ctx) => {
+  await ctx.reply();
+})
+
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
