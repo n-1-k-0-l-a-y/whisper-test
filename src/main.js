60e0662360972c8ed26599e881e8df578eb531c3
Nikolay <113996764+user175385803@users.noreply.github.com>
2023-11-20 17:46:40 +0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -24,6 +24,22 @@ bot.on(message('voice'), async (ctx) => {
   removeFile(oggPath)
   const text = await openai.transcription(mp3Path)
   await processTextToChat(ctx, text)
+  try {
+    await ctx.reply('Сообщение принял. Жду ответ от сервера...')
+    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
+    const userId = String(ctx.message.from.id)
+    const oggPath = await ogg.create(link.href, userId)
+    const mp3Path = await ogg.toMp3(oggPath, userId)
+
+    removeFile(oggPath)
+
+    const text = await openai.transcription(mp3Path)
+    await ctx.reply(`Ваш запрос: ${text}`)
+
+    await processTextToChat(ctx, text)
+  } catch (e) {
+    console.log(`Error while voice message`, e.message)
+  }
 })
 
 bot.on(message('text'), async (ctx) => {
