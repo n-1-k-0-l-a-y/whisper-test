13171f446cb0258ac97ec0ff6db95db9611e2819
Nikolay <113996764+user175385803@users.noreply.github.com>
2023-11-20 18:09:17 +0400

    upd nodemon

--- src/main.js
+++ src/main.js
@@ -16,36 +16,23 @@ bot.command('start', initCommand)
 
 bot.on(message('voice'), async (ctx) => {
   ctx.session ??= INITIAL_SESSION
-  await ctx.reply('Сообщение принял. Жду ответ от сервера...')
+
+  await ctx.reply('Ждем ответ от сервера...')
   const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
   const userId = String(ctx.message.from.id)
   const oggPath = await ogg.create(link.href, userId)
   const mp3Path = await ogg.toMp3(oggPath, userId)
+
   removeFile(oggPath)
+
   const text = await openai.transcription(mp3Path)
   await processTextToChat(ctx, text)
-  try {
-    await ctx.reply('Сообщение принял. Жду ответ от сервера...')
-    const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id)
-    const userId = String(ctx.message.from.id)
-    const oggPath = await ogg.create(link.href, userId)
-    const mp3Path = await ogg.toMp3(oggPath, userId)
-
-    removeFile(oggPath)
-
-    const text = await openai.transcription(mp3Path)
-    await ctx.reply(`Ваш запрос: ${text}`)
-
-    await processTextToChat(ctx, text)
-  } catch (e) {
-    console.log(`Error while voice message`, e.message)
-  }
 })
 
 bot.on(message('text'), async (ctx) => {
   ctx.session ??= INITIAL_SESSION
 
-  await ctx.reply('Сообщение принял. Жду ответ от сервера...')
+  await ctx.reply('Жду ответ от сервера')
   ctx.session.messages.push({
     role: openai.roles.USER,
     content: ctx.message.text
